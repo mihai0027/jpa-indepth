9831b553d7b3bc74a2fd9242db0437959753a01b
Mihai <mgold@MGold.lan>
2023-05-29 15:01:19 +0200

    creating project and configuring it with MySQL

---
+++ src/main/java/com/jpa/indepth/JpaIndepthApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.indepth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaIndepthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaIndepthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpa/indepth/JpaIndepthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.indepth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaIndepthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
