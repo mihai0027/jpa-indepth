c392efc53a7443644d125634adc0627918be74be
Mihai <mgold@MGold.lan>
2023-06-01 09:34:05 +0200

    example of paging and sorting for Course

--- src/test/java/com/jpa/indepth/repository/CourseRepositoryTest.java
+++ src/test/java/com/jpa/indepth/repository/CourseRepositoryTest.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import com.jpa.indepth.entity.Course;
 import com.jpa.indepth.entity.Teacher;
@@ -36,6 +40,39 @@ public class CourseRepositoryTest {
             .build();
 
         courseRepository.save(course);
+    }
+
+    @Test
+    public void findAllPagination() {
+        // first page with first 3 records
+        Pageable firstPageWithThreeRecords = PageRequest.of(0, 3);
+
+        // second page with 2 records
+        Pageable secondPageWithTwoRecords = PageRequest.of(1, 2);
+
+        List<Course> courses = courseRepository.findAll(firstPageWithThreeRecords).getContent();
+
+        long totalElements = courseRepository.findAll(secondPageWithTwoRecords).getTotalElements();
+        long totalPages = courseRepository.findAll(firstPageWithThreeRecords).getTotalPages();
+
+        System.out.println("totalPages = " + totalPages);
+        System.out.println("totalElements = " + totalElements);
+        System.out.println("courses = " + courses);
+
+    }
+
+    @Test
+    public void findAllWithSorting() {
+
+        // implement here sorting based on course title
+        //Pageable sortByTitle = PageRequest.of(0, 2, Sort.by("title"));
+        //Pageable sortByCreditDesc = PageRequest.of(0, 4, Sort.by("credit").descending());
+        Pageable sortByTitleAndCreditDesc = PageRequest.of(0, 2, Sort.by("title").descending()
+            .and(Sort.by("credit")));
+
+            List<Course> courses = courseRepository.findAll(sortByTitleAndCreditDesc).getContent();
+
+        System.out.println("courses sorted = " + courses);
     }
 
 }
